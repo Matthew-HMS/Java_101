b5d5b2655490f11fe09eba77d83df9d3bf0fb6ed
imsho <92440104+Matthew-HMS@users.noreply.github.com>
2022-05-22 18:08:45 +0800

    Add files via upload

---
+++ L9 Inheritance/Father.java
@@ -0,0 +1,5 @@
+public class Father {
+    public void habit() {
+        System.out.println("I like to play games");
+    }
+}
---
+++ L9 Inheritance/Son.java
@@ -0,0 +1,8 @@
+public class Son extends Father {
+    public void habit() {
+        System.out.println("I like to play paino");
+        super.habit();
+    }
+}
+    
+
